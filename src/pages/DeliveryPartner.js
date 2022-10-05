e15a821d86d09722c72cf66dff512e7bb8134b7d
Tun Hein <tminhein@gmail.com>
2022-10-05 12:14:31 -0700

    fix phone number

--- src/pages/DeliveryPartner.js
+++ src/pages/DeliveryPartner.js
@@ -83,7 +83,7 @@ const DeliveryPartner = () => {
                 </ol>
                 
                 <h4>Got some questions?</h4>
-                <p>We'd love to hear from you! You can call us at (415) 854-9524 or write us at <a href="mailto:contact@ragibull.com">contact@ragibull.com</a>
+                <p>We'd love to hear from you! You can call us at (408) 641 3741 or write us at <a href="mailto:contact@ragibull.com">contact@ragibull.com</a>
                 <br /><br />
                 </p>
                           
